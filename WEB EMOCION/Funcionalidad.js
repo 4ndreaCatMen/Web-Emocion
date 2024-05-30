eb4a1f7128536153c1ce12afb6167c7e224a2a8c
Andrea Catalán Menacho <145468932+4ndreaCatMen@users.noreply.github.com>
2024-05-31 00:18:28 +0200

    nuevos cambios para mejora del principal

--- WEB EMOCION/Funcionalidad.js
+++ WEB EMOCION/Funcionalidad.js
@@ -1,25 +1,38 @@
-// Función para detectar el desplazamiento hacia arriba
-function handleScroll() {
-    var lastScrollTop = 0;
-    var footer = document.querySelector('footer');
+    // Función para detectar el desplazamiento hacia arriba
+    function handleScroll() {
+        var lastScrollTop = 0;
+        var footer = document.querySelector('footer');
 
-    window.addEventListener('scroll', function() {
-        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        window.addEventListener('scroll', function() {
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-        if (scrollTop > lastScrollTop) {
-            // El usuario está desplazándose hacia abajo, mostramos el footer
-            footer.style.transform = 'translateY(0)';
-        } else {
-            // El usuario está desplazándose hacia arriba, ocultamos el footer
-            footer.style.transform = 'translateY(100%)';
-        }
+            if (scrollTop > lastScrollTop) {
+                // El usuario está desplazándose hacia abajo, mostramos el footer
+                footer.style.transform = 'translateY(0)';
+            } else {
+                // El usuario está desplazándose hacia arriba, ocultamos el footer
+                footer.style.transform = 'translateY(100%)';
+            }
 
-        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Para tratar con desplazamiento hacia arriba extremo
+            lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Para tratar con desplazamiento hacia arriba extremo
+        });
+    }
+
+    // Llamamos a la función al cargar la página
+    window.onload = function() {
+        handleScroll();
+    };
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var publicarBtn = document.getElementById('publicarBtn');
+    
+        publicarBtn.addEventListener('click', function(event) {
+            event.preventDefault(); // Evitar que el formulario se envíe
+    
+            // Mostrar el alert
+            alert('¡Blog publicado!');
+        });
     });
-}
-
-// Llamamos a la función al cargar la página
-window.onload = function() {
-    handleScroll();
-};
+    
+    
 
