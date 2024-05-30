43740f201f6271916ce08f7cd7d6bf7ea5676fa4
Andrea Catalán Menacho <145468932+4ndreaCatMen@users.noreply.github.com>
2024-05-30 22:15:21 +0200

    pagina principal acabada

---
+++ WEB EMOCION/Funcionalidad.js
@@ -0,0 +1,24 @@
+// Función para detectar el desplazamiento hacia arriba
+function handleScroll() {
+    var lastScrollTop = 0;
+    var footer = document.querySelector('footer');
+
+    window.addEventListener('scroll', function() {
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            // El usuario está desplazándose hacia abajo, mostramos el footer
+            footer.style.transform = 'translateY(0)';
+        } else {
+            // El usuario está desplazándose hacia arriba, ocultamos el footer
+            footer.style.transform = 'translateY(100%)';
+        }
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Para tratar con desplazamiento hacia arriba extremo
+    });
+}
+
+// Llamamos a la función al cargar la página
+window.onload = function() {
+    handleScroll();
+};
